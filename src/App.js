8701c4a0b047ac74ac7bd01225828c54d47c700f
Otar Qartlelishvili <qartlelishvilio@gmail.com>
2022-04-28 17:44:23 +0400

    created authentication page component and sign in form

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import './components/directory/directory.styles.scss'
 import Home from "./routes/home/home.component";
 import {Routes, Route} from "react-router-dom";
 import Navigation from "./routes/navigation/navigation.component";
-import SignIn from "./routes/sign-in/sign-in.component";
+import Authentication from "./routes/authentication/authentication.component";
 
 const App = () => {
     return (
         <Routes>
             <Route path='/' element={<Navigation />}>
                 <Route index element={<Home />}/>
-                <Route path='sign-in' element={<SignIn />}/>
+                <Route path='auth' element={<Authentication />}/>
             </Route>
         </Routes>
     );
